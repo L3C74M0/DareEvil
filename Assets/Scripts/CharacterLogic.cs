6b1b3d1212c56c36f79364c5487e6eeb6f78674d
David Lectamo <47828446+L3C74M0@users.noreply.github.com>
2022-05-20 11:42:32 +0200

    First commit

---
+++ Assets/Scripts/CharacterLogic.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLogic : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+    public float rotationSpeed = 200.0f;
+    private Animator animator;
+    public float x, y;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * movementSpeed);
+
+
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+    }
+}
