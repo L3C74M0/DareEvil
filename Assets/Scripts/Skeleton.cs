f06f49cdbf85d39cee46d955c1a4f5711bd43a7c
David Lectamo <47828446+L3C74M0@users.noreply.github.com>
2022-05-28 15:38:37 +0200

    Added Enemy

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour {
+
+    public int rutine;
+    public float chronometer;
+    public Animator animator;
+    public Quaternion angle;
+    public float degree;
+
+    public GameObject target;
+    
+    void Start() {
+        animator = GetComponent<Animator>();
+        target = GameObject.Find("erinys");
+    }
+
+    void Update() {
+        EnemyBehavior();
+    }
+
+    public void EnemyBehavior() {
+        if (Vector3.Distance(transform.position, target.transform.position) > 10) {
+            animator.SetBool("run", false);
+            chronometer += 1 * Time.deltaTime;
+            if (chronometer >= 4)
+            {
+                rutine = Random.Range(0, 2);
+                chronometer = 0;
+            }
+
+            switch (rutine)
+            {
+                case 0:
+                    animator.SetBool("walk", false);
+                    break;
+                case 1:
+                    degree = Random.Range(0, 360);
+                    angle = Quaternion.Euler(0, degree, 0);
+                    rutine++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angle, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    animator.SetBool("walk", true);
+                    break;
+            }
+        } else {
+            var lookPosition = target.transform.position - transform.position;
+            lookPosition.y = 0;
+            var rotation = Quaternion.LookRotation(lookPosition);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+            animator.SetBool("walk", false);
+            animator.SetBool("run", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+    }
+
+
+}
